24c4cc9affce7c362fc0a1f880a9eb5bda710e27
slam24 <luismartinez242011@gmail.com>
2017-04-10 14:54:32 -0600

    feat: Model form validation

--- boletin/forms.py
+++ boletin/forms.py
@@ -7,6 +7,16 @@ class RegModelForm(forms.ModelForm):
 		model = Registrado
 		fields = ["email","nombre"]
 
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		email_base, provider = email.split("@")
+		domain, extension = provider.split('.')
+		if not domain == 'USC':
+			raise forms.ValidationError("ingrese un correo de USC")
+		if not extension == "edu":
+			raise forms.ValidationError("Porfavor ingrese una direccion de correo valida ")
+		return email
+
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
 	email = forms.EmailField()
